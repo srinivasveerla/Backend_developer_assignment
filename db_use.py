86c61530fa3f8196822ee2c0e31764303d6cb943
srinivasveerla <67924501+srinivasveerla@users.noreply.github.com>
2020-10-20 15:51:22 +0530

    Create db_use.py

---
+++ db_use.py
@@ -0,0 +1,10 @@
+# Backend_developer_assignment
+import db_create as emp_d 
+emp_d.delete("*")
+emp_d.update(1,"A","B")
+emp_d.update(2,"B","C")
+emp_d.update(3,"D","B")
+emp_d.update(4,"C","")
+emp_d.update(5,"Q","")
+emp_d.display()
+
